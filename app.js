7726f10857d3a83528887aab31f74b1e5194382a
Jorge Verdugo <jorgeverdugoch@gmail.com>
2017-12-30 21:35:28 -0300

    Se crea la plantilla de archivo.

--- app.js
+++ app.js
@@ -9,14 +9,12 @@ var morgan = require('morgan');
 var config = require('./config');
 
 var app = express();
-//var db = mongoose.connect(config.database);
+//var db = mongoose.connect(config.db);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
-
-//app.set('superSecret', config.secret); // secret variable
 
 app.use('/autenticacion', require('./routes/autenticacion'));
 app.use('/estudiantes', require('./routes/estudiante'));
--- controllers/autenticacion.js
+++ controllers/autenticacion.js
@@ -5,43 +5,22 @@ var cheerio = require('cheerio');
 var jwt = require('jsonwebtoken');
 var config = require('../config');
 
+var Logger = require('../controllers/logger')
+
 exports.generar = function(req, res) {
-  var options = {
-    url: 'https://dirdoc.utem.cl/valida.php',
-    method: 'POST',
-    jar: request.jar(),
-    form: {
-      'tipo': 0,
-      'rut': req.query.rut,
-      'password': req.query.pass
-    }
-  };
+  Logger.dirdoc(req.query.rut, req.query.pass, function(jar) {
+    const payload = {
+      rut: req.query.rut,
+      pass: req.query.pass,
+    };
 
-  request(options, function(error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      var mensaje = $('body b').text();
+    var token = jwt.sign(payload, config.secreto, {expiresIn: 86400});
 
-      if (mensaje == "Bienvenido") {
-        const payload = {
-          rut: req.query.rut,
-          pass: req.query.pass,
-        };
-
-        var token = jwt.sign(payload, config.secret, {expiresIn: 86400});
-
-        res.status(200).json({
-          exito: true,
-          message: 'Token creada correctamente.',
-          token: token
-        });
-
-      } else {
-        console.log('Error al iniciar.');
-      }
-    } else {
-      console.log("Error al iniciar.");
-    }
+    res.status(200).json({
+      exito: true,
+      message: 'Token creada correctamente.',
+      token: token
+    });
   });
 }
 
