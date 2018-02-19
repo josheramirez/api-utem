3c32ee6070af9c3d25215b7a094bd62084168ce0
Jorge Verdugo <jorgeverdugoch@gmail.com>
2018-02-18 22:27:07 -0300

    Se mejora la ruta de autenticación

--- routes/autenticacion.js
+++ routes/autenticacion.js
@@ -7,7 +7,10 @@ var router = express.Router();
 var Auth = require('../controllers/autenticacion');
 
 router.post('/', function(req, res) {
-  Auth.generar(req, res);
+  res.status(200).send({
+    mensaje: "El token se generó correctamente",
+    token: Auth.generar(req, res)
+  });
 });
 
 module.exports = router;
