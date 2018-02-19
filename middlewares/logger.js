9541b33690db902abb14d32dbcc97d4a07f05eab
Jorge Verdugo <jorgeverdugoch@gmail.com>
2018-02-18 22:19:23 -0300

    Se arregla problema con respuestas

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -3,8 +3,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var Responses = require('../responses/clientError')
-
 exports.dirdoc = function(params, res, callback) {
   var options = {
     url: 'https://dirdoc.utem.cl/valida.php',
@@ -26,20 +24,21 @@ exports.dirdoc = function(params, res, callback) {
         case 'Bienvenido':
           callback(options.jar);
           break;
-        case 'No ha ingresado RUT o Password. Intentelo nuevamente.':
-          Responses.r400(1, res);
-          break;
-        case 'El password ingresado no es válido. Intentelo nuevamente':
-          Responses.r401(1, res);
-          break;
-        case 'Ud. no está autorizado para entrar a gestión.':
-          Responses.r402(1, res);
-          break;
-        default:
-          Responses.r500(1, res);
+        // case 'No ha ingresado RUT o Password. Intentelo nuevamente.':
+        //   Responses.r400(1, res);
+        //   break;
+        // case 'El password ingresado no es válido. Intentelo nuevamente':
+        //   Responses.r401(1, res);
+        //   break;
+        // case 'Ud. no está autorizado para entrar a gestión.':
+        //   Responses.r402(1, res);
+        //   break;
+        // default:
+        //   Responses.r500(1, res);
       }
-    } else {
-      Responses.r503(1, res);
     }
+    // } else {
+    //   Responses.r503(1, res);
+    // }
   });
 }
